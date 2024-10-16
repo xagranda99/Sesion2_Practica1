1464766f2354c77099745e48cb62c72bd9d9853c
xagranda <xagranda@gmail.com>
2024-10-16 18:17:14 +0200

    Initial Commit

---
+++ Sesion2_Practica1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sesion2_Practica1
+//
+//  Created by Xavier Granda Paz on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
