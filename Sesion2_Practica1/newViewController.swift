406a29b02bebac3b9c5746a732c061c5e67234fb
xagranda <xagranda@gmail.com>
2024-10-16 18:24:03 +0200

    Subir proyecto

---
+++ Sesion2_Practica1/newViewController.swift
@@ -0,0 +1,29 @@
+//
+//  newViewController.swift
+//  Sesion2_Practica1
+//
+//  Created by Xavier Granda Paz on 16/10/24.
+//
+
+import UIKit
+
+class newViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
